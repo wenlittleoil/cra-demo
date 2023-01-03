fca88bf5be05addace7e7305366d33e7baa6674c
shaoyouwen <shaoyouwen@chatlabs.com>
2023-01-03 15:19:32 +0800

    feat: login page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
 
-const NotFound = () => <div>404 not found.</div>;
+import { Result } from 'antd';
+
+const NotFound = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+  />
+);
 
 export default NotFound;
--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import './index.scss';
 
-const LoginPage = () => {
+const LoginPage: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div>
-      登录页
+    <div className="login-page">
+      <div className="form-container">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+            <Checkbox>记住密码</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
