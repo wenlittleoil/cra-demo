4d257e4440beca791b58e335212dba36cdc024a1
shaoyouwen <shaoyouwen@chatlabs.com>
2023-02-07 01:01:54 +0800

    feat: 支持哈希路由

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const path = require('path');
 const app = express();
 
 // app.use(express.static(path.join(__dirname, 'build')));
-app.use('/wen-base', express.static(path.join(__dirname, 'build')));
+app.use('/wen-base', express.static(path.join(__dirname, 'build'))); // 基础资源公共路径
 
+// 仅支持哈希路由
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// 支持h5历史路由
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`server is listening at http://localhost:${port}`);
+  console.log(`server is listening at ${port}`);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   BrowserRouter,
+  HashRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -12,15 +13,18 @@ import DashboardPage from 'containers/dashboard';
 
 console.log('process.env.REACT_APP_ENV', process.env.REACT_APP_ENV)
 
+const isHashRouter = false;
+
 function App() {
+  const Router = isHashRouter ? HashRouter : BrowserRouter;
   return (
-    <BrowserRouter basename='/wen-base'>
+    <Router basename='/wen-base'>
       <Routes>
         <Route path="dashboard/*" element={<DashboardPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
