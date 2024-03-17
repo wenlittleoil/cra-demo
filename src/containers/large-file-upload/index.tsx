960eabceda38ec35dea3968fb494eb27270ddc4c
shaoyouwen <shaoyouwen@chatlabs.com>
2024-03-17 11:58:21 +0800

    feat: 完善前端分片上传逻辑

--- src/containers/large-file-upload/index.tsx
+++ src/containers/large-file-upload/index.tsx
@@ -1,51 +1,83 @@
 import { useState } from 'react';
 import './index.scss';
 import axios from 'axios';
-import { message } from 'antd';
+import { Progress, message } from 'antd';
 import { generateRandomString } from 'utils/tool';
 
 const id = "upload-file-engine";
 
+enum EUploadStatus {
+  ready = 'ready',
+  uploading = 'uploading',
+  paused = 'paused',
+  completed = 'completed',
+}
+
+type FileWithUploadInfo = {
+  originalFile: File,
+  // 标示上传文件唯一性的uuid，通常使用文件名称和内容的哈希值
+  fileId: string,
+  // 文件上传状态
+  uploadStatus: EUploadStatus,
+  // 文件上传进度，从0到1，小数点后两位精度
+  uploadProgress: number
+}
+
 const Index = () => {
-  const [curFiles, setCurFiles] = useState<File[]>([]);
+  const [curFiles, setCurFiles] = useState<FileWithUploadInfo[]>([]);
   const onChange = () => {
     const input: HTMLInputElement = document.querySelector(`#${id}`);
     const fileList: FileList = input.files;
-    const files: File[] = [].slice.call(fileList);
-    setCurFiles([...curFiles, ...files]);
-    console.log('测试', files)
+    const originalFiles: File[] = [].slice.call(fileList);
+    const customFiles = originalFiles.map((item: File) => ({
+      originalFile: item,
+      fileId: generateRandomString(16), // 这里为了演示，先简单使用随机生成ID
+      uploadStatus: EUploadStatus.ready,
+      uploadProgress: 0,
+    }))
+    setCurFiles([...curFiles, ...customFiles]);
+    console.log('测试', customFiles)
   }
 
-  const beginUpload = (file: File) => {
-    // 标示上传文件唯一性的uuid，通常使用文件名称和内容的哈希值，这里为了演示，先简单使用随机生成ID
-    const fileId = generateRandomString(16);
+  const beginUpload = (customFile: FileWithUploadInfo) => {
+    const file: File = customFile.originalFile;
 
     // 将文件切成一个个片段逐个上传到服务端
     const chunkSize = 5 * 1000 * 1000; // 每个片段的大小为5M并转换成byte
     const chunkTotal = Math.ceil(file.size / chunkSize); // 总片段数
     let chunkNo = 1; // 片段编号，从1开始上传，直到chunkTotal结束
 
-    const upload = (_chunkNo: number) => {
-
-      const formData = new FormData();
-
+    const uploadChunk = (_chunkNo: number) => {
       // 例如第一个分片[0, 5000000)，第二个分片[5000000, 10000000)，第三个分片[10000000, 15000000)，以此类推
       const byteStartIndex = (_chunkNo - 1) * chunkSize;
       const byteEndIndex = _chunkNo * chunkSize;
       const chunk = file.slice(byteStartIndex, byteEndIndex); 
 
+      const formData = new FormData();
       formData.append('chunk', chunk);
       formData.append('chunkNo', String(chunkNo));
       formData.append('chunkTotal', String(chunkTotal));
-      formData.append('fileId', fileId);
+      formData.append('fileId', customFile.fileId);
 
       axios.post('/api/upload', formData).then(res => {
         console.log('文件上传', res)
         if (res.data === 'success') {
           chunkNo++;
+
+          setCurFiles(prevList => prevList.map(item => {
+            if (item.fileId === customFile.fileId) {
+              return {
+                ...item,
+                uploadStatus: EUploadStatus.uploading,
+                uploadProgress: Math.floor((chunkNo / chunkTotal) * 100) / 100,
+              }
+            }
+            return item;
+          }));
+
           if (chunkNo <= chunkTotal) {
             // 继续上传下一个分片
-            upload(chunkNo);
+            uploadChunk(chunkNo);
           } else {
             // 最后一个分片完成上传，意味着上传结束
             message.success(`文件${file.name}上传成功`);
@@ -76,12 +108,43 @@ const Index = () => {
       <div className="selected">
         <div>已经选中{curFiles.length}个文件</div>
         <div className="selected-file-list">
-          {curFiles.map((file, index) => {
+          {curFiles.map((customFile, index) => {
+            const file = customFile.originalFile;
+            const status = customFile.uploadStatus;
+            const pgStyle = { width: '300px' }
             return (
               <div key={index} className="selected-file-item">
                 <span>{file.name}</span>
                 <span>{file.size}(byte)</span>
-                <button onClick={() => beginUpload(file)}>上传到远程</button>
+
+                {status === EUploadStatus.ready && (
+                  <button onClick={() => beginUpload(customFile)}>上传到远程</button>
+                )}
+
+                {/* 测试分片上传 */}
+                {status !== EUploadStatus.ready && (
+                  <Progress style={pgStyle} percent={customFile.uploadProgress * 100} />
+                )}
+
+                {/* 测试断点续传 */}
+                {/* {status === EUploadStatus.uploading && (
+                  <div>
+                    <Progress percent={customFile.uploadProgress * 100} />
+                    <button>暂停</button>
+                  </div>
+                )}
+                {status === EUploadStatus.paused && (
+                  <div>
+                    <Progress percent={customFile.uploadProgress * 100} />
+                    <button>继续</button>
+                  </div>
+                )}
+                {status === EUploadStatus.completed && (
+                  <div>
+                    <Progress percent={customFile.uploadProgress * 100} />
+                    <div>上传完成！</div>
+                  </div>
+                )} */}
               </div>
             )
           })}
