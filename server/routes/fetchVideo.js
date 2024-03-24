c69adf74fdb103a812c79e343ad0504ef571abb6
shaoyouwen <shaoyouwen@chatlabs.com>
2024-03-24 17:54:03 +0800

    feat: 临时提交

--- server/routes/fetchVideo.js
+++ server/routes/fetchVideo.js
@@ -2,10 +2,69 @@ const {
   beBasePath,
 } = require('../constant');
 const path = require('path');
+const fs = require('fs');
 
 module.exports = app => {
-  // 获取视频时
-  app.use("/api/media/test.mp4", function (req, res) {
-    res.sendFile(path.join(beBasePath, 'static', 'videos', 'test.mp4'));
+  const mp4FilePath = path.join(beBasePath, 'static', 'videos', 'test.mp4');
+
+  // 获取整个视频
+  app.get("/api/media/testmp4", function (req, res) {
+    console.log('获取整个视频')
+    res.sendFile(mp4FilePath);
   });
+
+  // 获取视频某个片段（通过http标准头部Range）
+  app.get("/api/media/testmp4/part", function (req, res) {
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+      res.status(400).send("Requires Range header");
+    }
+  
+    const videoPath = mp4FilePath;
+    const videoSize = fs.statSync(mp4FilePath).size;
+
+    console.log('获取文件字节范围', range, videoSize)
+  
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+  
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+  
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+  
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+  });
+
+  // 获取视频某个片段（通过http自定义请求）
+  app.get("/api/media/testmp4/part-v2", function (req, res) {
+    const {
+      start,
+      end,
+    } = req.query;
+    const _start = Number(start);
+    const videoSize = fs.statSync(mp4FilePath).size;
+    const _end = Math.min(Number(end), videoSize - 1);
+
+    const videoStream = fs.createReadStream(mp4FilePath, { 
+      start: _start, 
+      end: _end,
+    });
+    console.log('分段获取视频片段', _start, _end)
+    videoStream.pipe(res);
+  });
+
 }
--- src/containers/video/index.tsx
+++ src/containers/video/index.tsx
@@ -7,7 +7,11 @@ const VideoPage = () => {
   useEffect(() => {
     const video = document.querySelector("#stream-media-video") as HTMLMediaElement;
 
-    const assetURL = "/api/media/test.mp4";
+    // 通过xhr请求获取整个视频文件
+    // const assetURL = "/api/media/testmp4";
+    // 通过xhr请求获取部分视频片段(视频总共1.5M，先获取前面1M部分，接着获取后面0.5M部分)
+    const assetURL = "/api/media/testmp4/part-v2?start=0&end=1000000"; 
+
     // 使用Bento4下bin提供的命令行工具分析mp4文件
     // Need to be specific for Blink regarding codecs
     // ./mp4info test.mp4 | grep Codec
@@ -31,9 +35,15 @@ const VideoPage = () => {
       const playBtn = document.querySelector('.play-btn');
       const init = () => {
         const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+        sourceBuffer.mode = 'segments';
         fetchAB(assetURL, function (buf) {
+          sourceBuffer.addEventListener('error', (e) => {
+            console.error("sourceBuffer error", e);
+          });
+          
           sourceBuffer.addEventListener("updateend", function (_) {
-            mediaSource.endOfStream();
+            // mediaSource.endOfStream();
+
             video.play();
             videoInited.current = true;
             playBtn.removeEventListener("click", init);
@@ -41,6 +51,14 @@ const VideoPage = () => {
   
           });
           sourceBuffer.appendBuffer(buf);
+
+          // 如果是分段获取，则接着获取后面0.5M部分
+          if (assetURL.includes("part-v2")) {
+            fetchAB("/api/media/testmp4/part-v2?start=1000000&end=2000000", function (secondBuf) {
+              sourceBuffer.appendBuffer(secondBuf);
+            });
+          }
+
         });
       }
       playBtn.addEventListener("click", init)
@@ -78,11 +96,17 @@ const VideoPage = () => {
 
   return (
     <div>
-      {/* 流媒体 */}
+      {/* 1、流媒体(通过xhr请求获取视频片段，视频已经提前被分割成片段放置在服务器磁盘中) */}
       <video 
         id='stream-media-video'
         // muted 
         // loop
+        width="400px"
+        height="400px"
+        style={{
+          objectFit: 'contain',
+        }}
+        controls
       />
       <div>
         <button className='play-btn' onClick={handleClickPlay}>播放</button>
@@ -90,7 +114,11 @@ const VideoPage = () => {
         <button className='back-btn' onClick={handleClickBack}>回到第20s</button>
       </div>
 
-      {/* 静态媒体(边下边播) */}
+      {/* 2、流媒体(通过http请求获取视频片段，视频并没有被分割成片段，
+        而是以完整文件的形式存在于服务器磁盘中，通过http头部Content-Range控制返回视频片段) */}
+      {/* <video id="videoPlayer" width="70%" controls autoPlay muted src="/api/media/testmp4/part" /> */}
+
+      {/* 3、本域内的静态媒体(边下边播) */}
       {/* <video 
         src='/wen-base/media/test-static.webm' 
         autoPlay 
@@ -99,8 +127,10 @@ const VideoPage = () => {
           marginTop: '20px',
         }} 
       /> */}
+
+      {/* 4、外域内的静态媒体(边下边播) */}
       {/* <video 
-        src='https://graff-1253185145.cos.ap-guangzhou.myqcloud.com/test-static-1708072702709.mp4' 
+        src='https://graff-1253185145.cos.ap-guangzhou.myqcloud.com/test-static-1708072702709-v2.mp4' 
         autoPlay 
         muted 
         style={{
