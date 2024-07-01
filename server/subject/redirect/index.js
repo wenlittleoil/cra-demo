db4025f90e53b34bbb784f9f2cf3be92b2b41651
shaoyouwen <shaoyouwen@chatlabs.com>
2024-07-01 18:40:47 +0800

    feat: 网页级别的重定向

--- server/subject/redirect/index.js
+++ server/subject/redirect/index.js
@@ -12,6 +12,28 @@ app.get("/", (req, res) => {
   res.sendFile(file);
 });
 
+// 一、页面访问的重定向（通过浏览器直接输入url访问，会发生网页级别的重定向）
+app.get("/page/1", (req, res) => {
+  console.log('访问了/page/1')
+  return res.redirect("/page/2")
+});
+
+app.get("/page/2", (req, res) => {
+  console.log('访问了/page/2')
+  res.send("This is Page Two")
+});
+
+app.get("/page/3", (req, res) => {
+  console.log('访问了/page/3')
+  return res.redirect("http://www.baidu.com");
+});
+
+app.get("/page/4", (req, res) => {
+  console.log('访问了/page/4')
+  return res.redirect("http://localhost:3002/getcookie");
+});
+
+// 二、API异步请求的重定向（通过浏览器fetch或xhr请求）
 app.get("/api/1", (req, res) => {
   console.log('访问了/api/1')
   // 请求会先返回一个302 Location到客户端浏览器，然后浏览器再自动重新向Location地址发起另一个请求
